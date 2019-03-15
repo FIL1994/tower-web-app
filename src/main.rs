eed469d734a99325337b5a03510dd3645b664243
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-03-14 22:06:21 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate tower_web;
+
+use tower_web::ServiceBuilder;
+
+#[derive(Clone, Debug)]
+struct HelloWorld;
+
+#[derive(Debug, Extract)]
+struct MyData {
+    foo: usize,
+    bar: Option<String>,
+}
+
+/// This will be the JSON response
+#[derive(Response)]
+struct HelloResponse {
+    message: &'static str,
+}
+
+impl_web! {
+    impl HelloWorld {
+        #[get("/")]
+        fn hello_world(&self) -> Result<String, ()> {
+            Ok("Hello world".to_string())
+        }
+
+        #[get("/hello/:name")]
+        fn greet(&self, name: String) -> Result<String, ()> {
+            Ok(format!("Hello, {}", name))
+        }
+
+        #[post("/data")]
+        fn data(&self, body: MyData) -> Result<String, ()> {
+            Ok(format!("Hello, {:?}", body))
+        }
+
+        #[get("/json")]
+        #[content_type("json")]
+        fn hello_world2(&self) -> Result<HelloResponse, ()> {
+            Ok(HelloResponse {
+                message: "hello world",
+            })
+        }
+    }
+}
+
+pub fn main() {
+    let addr = "127.0.0.1:8080".parse().expect("Invalid address");
+    println!("Listening on http://{}", addr);
+
+    ServiceBuilder::new()
+        .resource(HelloWorld)
+        .run(&addr)
+        .unwrap();
+}
